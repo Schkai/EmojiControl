2cc2e84cd7805bad2a7822c684b52d62abab0353
aend <a.h.1974@gmx.de>
2017-04-15 16:11:02 +0200

    started with edit page

--- app/src/main/java/de/ur/emojicontrol2/Bearbeiten.java
+++ app/src/main/java/de/ur/emojicontrol2/Bearbeiten.java
@@ -2,14 +2,25 @@ package de.ur.emojicontrol2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
 
 public class Bearbeiten extends AppCompatActivity
 {
 
+    CalendarView calendar;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bearbeiten);
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener()
+        {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth)
+            {
+
+            }
+        });
     }
 }
